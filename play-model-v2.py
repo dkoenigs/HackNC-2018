1739d922d670029678e8726c4b4b9ad82550ed0e
ruiwu08 <33375962+ruiwu08@users.noreply.github.com>
2018-10-07 09:38:19 -0400

    Add files via upload
    Refined version of play-model.py. decision function might actually work now

---
+++ play-model-v2.py
@@ -0,0 +1,177 @@
+import numpy as np
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import tree
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix  
+
+def semicolon(str):
+    if ";" not in str:
+        return True
+    else:
+        return False
+
+# def buildModel():
+#     data = pd.read_csv('googleplaystore.csv')
+
+#     newSize = []
+
+#     for x in data['Size']:
+#         if(x[-1:] == 'M'):
+#             newSize.append(float(x[:-1]))
+#         elif(x[-1:] == 'k'):
+#             newSize.append(float(x[:-1]))
+#         elif(x[-1:] == 'G'):
+#             newSize.append(float(x[:-1]))
+#         else:
+#             newSize.append(9999999)
+
+#     data['newSize'] = newSize
+#     data.drop(data.columns[2], axis=1, inplace=True)
+#     data = data[data['newSize'] != 9999999]
+
+#     catDummy = pd.get_dummies(data['Category'])
+#     typeDummy = pd.get_dummies(data['Type'])
+#     crDummy = pd.get_dummies(data['Content Rating'])
+#     genreDummy = pd.get_dummies(data['Genres'])
+
+
+
+#     genreList = genreDummy.columns
+#     newGenreList = []
+#     for elt in genreList:
+#         if(semicolon(elt)):
+#             newGenreList.append(elt)
+
+#     genreDummy = genreDummy[newGenreList]
+
+#     data = pd.concat([data, catDummy, typeDummy, crDummy, genreDummy], axis = 1)
+#     cols = [1,2,3,4]
+
+#     data.drop(data.columns[cols], axis = 1, inplace=True)
+
+#     X = data.values[:, 1:]
+#     Y = data.values[:, 0]
+
+#     X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.3, random_state = 100)
+
+#     scaler = StandardScaler()  
+#     scaler.fit(X_train)
+
+#     X_train = scaler.transform(X_train)  
+#     X_test = scaler.transform(X_test)  
+
+#     classifier = KNeighborsClassifier(n_neighbors=40)  
+#     classifier.fit(X_train, y_train) 
+#     y_pred = classifier.predict(X_test)  
+
+
+def decision(size, cat, type, cr, genre):
+    data = pd.read_csv('googleplaystore.csv')
+
+    newSize = []
+
+    for x in data['Size']:
+        if(x[-1:] == 'M'):
+            newSize.append(float(x[:-1]))
+        elif(x[-1:] == 'k'):
+            newSize.append(float(x[:-1]))
+        elif(x[-1:] == 'G'):
+            newSize.append(float(x[:-1]))
+        else:
+            newSize.append(9999999)
+
+    data['newSize'] = newSize
+    data.drop(data.columns[2], axis=1, inplace=True)
+    data = data[data['newSize'] != 9999999]
+
+    catDummy = pd.get_dummies(data['Category'])
+    typeDummy = pd.get_dummies(data['Type'])
+    crDummy = pd.get_dummies(data['Content Rating'])
+    genreDummy = pd.get_dummies(data['Genres'])
+
+
+
+    genreList = genreDummy.columns
+    newGenreList = []
+    for elt in genreList:
+        if(semicolon(elt)):
+            newGenreList.append(elt)
+
+    genreDummy = genreDummy[newGenreList]
+
+    data = pd.concat([data, catDummy, typeDummy, crDummy, genreDummy], axis = 1)
+    cols = [1,2,3,4]
+
+    data.drop(data.columns[cols], axis = 1, inplace=True)
+
+    X = data.values[:, 1:]
+    Y = data.values[:, 0]
+
+    X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.3, random_state = 100)
+
+    scaler = StandardScaler()  
+    scaler.fit(X_train)
+
+    X_train = scaler.transform(X_train)  
+    X_test = scaler.transform(X_test)  
+
+    classifier = KNeighborsClassifier(n_neighbors=3)  
+    classifier.fit(X_train, y_train) 
+    y_pred = classifier.predict(X_test)
+
+    print(classification_report(y_test, y_pred))
+
+    def catMaker(str):
+        catList = []
+        for i in range(0, len(catDummy.columns)):
+            if str == catDummy.columns[i] :
+                catList.append(1)
+            else:
+                catList.append(0)
+        return catList
+
+    def typeMaker(str):
+        typeList = []
+        for i in range(0, len(typeDummy.columns)):
+            if str == typeDummy.columns[i]:
+                typeList.append(1)
+            else:
+                typeList.append(0)
+        return typeList
+
+    def crMaker(str):
+        crList = []
+        for i in range(0, len(crDummy.columns)):
+            if str == crDummy.columns[i]:
+                crList.append(1)
+            else:
+                crList.append(0)
+        return crList
+
+    def genreMaker(str):
+        genreList = []
+        for i in range(0, len(genreDummy.columns)):
+            if str == genreDummy.columns[i]:
+                genreList.append(1)
+            else:
+                genreList.append(0)
+        return genreList
+
+    finalList = [size]
+    catList = catMaker(cat)
+    typeList = typeMaker(type)
+    crList = crMaker(cr)
+    genreList = genreMaker(genre)
+    finalList = finalList + catList + typeList + crList + genreList
+
+    result = classifier.predict([finalList])
+    return result[0]
+    
+
+
+
+print(decision(0.2, "BUSINESS", "Paid", "Everyone", "Business"))
